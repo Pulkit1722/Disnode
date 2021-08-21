b55865876a50bba6520704c4daaf4942a08811db
Pulkit1772 <pulkit260599@gmail.com>
2021-08-21 15:49:45 +0530

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+});
+
+app.listen(3000,()=>{
+    console.log("Server has started");
+});
